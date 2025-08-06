ada7a7acebcc5c9081cb865a390d3f95f4f9e1de
David <david.markus.olsson@gmail.com>
2025-08-06 17:16:03 +0200

    rename eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["./packages/eslint-config/index.js"],
+  extends: ["@chess-app/eslint-config/index.js"],
 };
