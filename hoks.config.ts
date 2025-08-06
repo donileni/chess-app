15bb14099cd0fddbec137540b821c302fd395d01
Anton Ödman <anton.odman@gmail.com>
2025-08-06 21:49:54 +0200

    chore: format all files with new config

--- hoks.config.ts
+++ hoks.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "hoks";
 
 export default defineConfig({
-  installOnLockChange: true,
-  branchName: {
-    pattern: "^(feature/.+|bugfix/.+|hotfix/.+|release/.+|main|develop)$",
-    message:
-      "Branch name must start with 'feature/', 'bugfix/', 'hotfix/', 'release/'",
-  },
-  staged: {
-    "*": "npm run format",
-  },
-  enforceConventionalCommits: true,
+    installOnLockChange: true,
+    branchName: {
+        pattern: "^(feature/.+|bugfix/.+|hotfix/.+|release/.+|main|develop)$",
+        message:
+            "Branch name must start with 'feature/', 'bugfix/', 'hotfix/', 'release/'",
+    },
+    staged: {
+        "*": "npm run format",
+    },
+    enforceConventionalCommits: true,
 });
--- packages/ui/eslint.config.cjs
+++ packages/ui/eslint.config.cjs
@@ -6,22 +6,22 @@ const js = require("@eslint/js");
 const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
 });
 
 module.exports = defineConfig([
-  {
-    extends: compat.extends("@chess-app/eslint-config/index.js"),
+    {
+        extends: compat.extends("@chess-app/eslint-config/index.js"),
 
-    languageOptions: {
-      parser: tsParser,
+        languageOptions: {
+            parser: tsParser,
 
-      parserOptions: {
-        project: true,
-      },
+            parserOptions: {
+                project: true,
+            },
+        },
     },
-  },
-  globalIgnores(["**/.eslintrc.cjs"]),
+    globalIgnores(["**/.eslintrc.cjs"]),
 ]);
