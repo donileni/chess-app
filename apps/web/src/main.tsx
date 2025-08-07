b610fdd0feb90de9a97a6a76a70c824ce9da954d
David <david.markus.olsson@gmail.com>
2025-08-07 10:53:13 +0200

    feat: added tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,25 +1,17 @@
 import { createRoot } from "react-dom/client";
-import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./client/routeTree.gen";
 
-const App = () => (
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
-    </div>
-  </div>
-);
+const router = createRouter({
+    routeTree,
+});
+
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+const App = () => <RouterProvider router={router} />;
 
 createRoot(document.getElementById("app")!).render(<App />);
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        tanstackRouter({
+            routesDirectory: "./src/client/routes/",
+            generatedRouteTree: "./src/client/routeTree.gen.ts",
+            target: "react",
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tsconfigPaths(),
+    ],
 });
